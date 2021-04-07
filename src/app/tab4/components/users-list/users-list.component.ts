888e43b65d9f0291a6ae6268c022ce361b0061b4
marcodst <marcodst13@gmail.com>
2021-04-07 18:23:08 +0200

    align to main

--- src/app/tab4/components/users-list/users-list.component.ts
+++ src/app/tab4/components/users-list/users-list.component.ts
@@ -1,7 +1,10 @@
-import { User } from 'src/app/models/User';
-import { Component, OnInit } from '@angular/core';
-import { UsersService } from 'src/app/services/users.service';
-import { ModalController } from '@ionic/angular';
+import {presentToast} from '../../../commons/utils';
+import {SocialService} from '../../../services/social.service';
+import {User} from 'src/app/models/User';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from 'src/app/services/users.service';
+import {ModalController} from '@ionic/angular';
+import {Follower} from 'src/app/models/Follower';
 
 @Component({
   selector: 'app-users-list',
@@ -11,21 +14,35 @@ import { ModalController } from '@ionic/angular';
 export class UsersListComponent implements OnInit {
   User: any;
 
-  constructor(private userService: UsersService, public modalController: ModalController) { }
+  constructor(private userService: UsersService, public modalController: ModalController, private socialService: SocialService) {
+  }
 
-  users: User[] = []
-  
+  users: User[] = [];
+  allFollowed: Follower[] = [];
 
-  getAll = async () => this.users= await this.userService.all();
+  getAll = async () => this.users = await this.userService.all();
 
-  dismiss() {
-    this.modalController.dismiss({
-      'dismissed': true
+  async dismiss() {
+    await this.modalController.dismiss({
+      dismissed: true
     });
+  }
 
+  getAllFollowed = async () => this.allFollowed = await this.socialService.allFollowed(localStorage.getItem('user_id'));
+
+  removeJustFollowed = (id: string) => this.allFollowed.find(({_id}: Follower) => _id === id);
+
+  addFriend = async (userIdToFollow: string, userNameToFollow: string) => {
+    try {
+      await this.socialService.add(localStorage.getItem('user_id'), {userIdToFollow});
+    } catch (error: any) {
+      return presentToast(error);
+    }
+    await presentToast(`${userNameToFollow} added!`);
   }
-  ngOnInit():void {
-    this.getAll().then(() => console.log(this.users))
+
+  async ngOnInit() {
+    await this.getAll();
+    await this.getAllFollowed();
   }
-  
 }
